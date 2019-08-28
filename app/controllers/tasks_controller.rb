5df510779df067cd3361f19df50e64397f3fd25d
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-08-28 23:27:15 +0900

    add:Taskの新規登録機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,24 @@ class TasksController < ApplicationController
     @task = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
 
+  def create
+    @task = Task.new(task_params)
+    binding.pry
+    if @task.save
+     redirect_to tasks_path, notice: "タスクを作成しました"
+    else
+     render 'new'
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title,:details,:deadline,:priority,:status)
+  end
 
 end
