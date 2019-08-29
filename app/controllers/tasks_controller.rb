0ff482bd3ae2af1ce369a1f179dce31cc020f1c1
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-08-30 00:12:28 +0900

    add:タスクの編集機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:edit,:update]
 
   def index
     @task = Task.all
@@ -18,10 +19,25 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+     redirect_to tasks_path, notice: "タスクを更新しました！"
+    else
+     render 'edit'
+    end
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title,:details,:deadline,:priority,:status)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
