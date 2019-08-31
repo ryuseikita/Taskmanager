1fcb0a9b69bdc9f8f09bdf9b168cca96ab50082f
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-08-31 23:58:50 +0900

    add:バリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :details, presence: true
+  validates :deadline, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
 end
