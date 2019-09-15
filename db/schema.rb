3c84c3e0858da7933ce97879a697d7edf3ddf241
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-09-16 00:52:19 +0900

    add：statusにindexに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_101204) do
+ActiveRecord::Schema.define(version: 2019_09_15_144256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_101204) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title", null: false
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
