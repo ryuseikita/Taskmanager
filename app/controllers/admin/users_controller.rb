5c0ed4e651a20592c77c969ec9d1f9570730c819
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-10-26 12:13:08 +0900

    add:管理画面でのユーザ削除機能の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,39 @@
 class Admin::UsersController < ApplicationController
+  before_action :admin_check
+  before_action :set_user, only:[:edit,:update,:show,:destroy]
   def index
     @user = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    #未修正
+  end
+
+  def show
+  end
+
+  def destroy
+    @user.delete
+    redirect_to admin_users_path,notice: "ユーザを削除しました"
+  end
+
+
+  private
+  
+  def admin_check
+    if logged_in?
+      unless current_user.admin
+        redirect_to tasks_path ,notice: "管理者権限を持ったユーザでログインをしてください。"
+      end
+    elsif
+      redirect_to tasks_path ,notice: "ログインをしてください。"
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
