bbd5e3559b8956fce63e3aa286b585c29e9a23aa
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-11-12 23:19:27 +0900

    add:ラベル実装のテスト追加

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
-  factory :label do
-    name { "MyString" }
+  factory :label01,class: Label do
+    id{10}
+    name { "ラベルテスト001" }
+  end
+  factory :label02,class: Label do
+    id{11}
+    name { "ラベルテスト002" }
+  end
+  factory :label03,class: Label do
+    id{12}
+    name { "ラベルテスト003" }
   end
 end
--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :task do
-      id{'2'}
+      id{'10'}
       title { 'test02' }
       details { 'test02_details' }
       deadline { '2019/09/01' }
@@ -10,7 +10,7 @@ FactoryBot.define do
       user_id {'10'}
     end
     factory :second_task, class: Task do
-      id{'3'}
+      id{'11'}
       title { 'test03' }
       details { 'test03_details' }
       deadline { '2019/09/03' }
@@ -20,7 +20,7 @@ FactoryBot.define do
       user_id {'10'}
     end
     factory :third_task, class: Task do
-      id{'4'}
+      id{'12'}
       title { 'test04' }
       details { 'test04_details' }
       deadline { '2019/09/04' }
@@ -31,7 +31,7 @@ FactoryBot.define do
     end
   
     factory :task_another, class: Task do
-      id{'5'}
+      id{'13'}
       title { 'test05' }
       details { 'test05_details' }
       deadline { '2019/09/02' }
@@ -41,7 +41,7 @@ FactoryBot.define do
       user_id {'10'}
     end
     factory :quattro_task, class: Task do
-      id{'6'}
+      id{'14'}
       title { 'test06' }
       details { 'test06_details' }
       deadline { '2019/09/01' }
@@ -51,7 +51,7 @@ FactoryBot.define do
       user_id {'10'}
     end
     factory :zero_task, class: Task do
-      id{'7'}
+      id{'15'}
       title { 'test00' }
       details { 'test00_details' }
       deadline { '2019/08/30' }
