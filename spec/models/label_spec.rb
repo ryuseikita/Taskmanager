9d15544af3efd29dad1dc2c66e05c0d5ae6b29cf
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-11-14 02:55:03 +0900

    add:バリデーションテスト追加

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "nameがからの場合バリデーションエラーになることを確認する" do
+    label = Label.create(id:'10',name:'')
+    expect(label).not_to be_valid
+  end
 end
